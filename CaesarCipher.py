0fd63c43a788302734b4fd0ed716306fc6e37a73
John Hashem III <johnhashemiii@Johns-MacBook-Pro.local>
2022-02-27 22:28:19 -0500

    anagrams

---
+++ CaesarCipher.py
@@ -0,0 +1,17 @@
+
+
+# def caesarCipherEncryptor(string, key):
+#     letters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+#     for idx, char in enumerate(string):
+#         if char in letters:
+#     	    print(char[idx])
+
+
+# string = "abc"
+# caesarCipherEncryptor(string, 2)
+
+
+def greet(s):
+  print(f"hey {s}")
+
+greet("john")
